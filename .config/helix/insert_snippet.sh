c0ae4c60173308e63e66f62c3e55711bae842d2f
Augusto Coronel <aoc@getgoogleoff.me>
2025-10-21 16:46:55 -0300

    deprecate(helix): remove insert_snippet bash script

--- .config/helix/insert_snippet.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SNIPPET_DIR="$HOME/.config/helix/snippets/"
-
-file=$(find "$SNIPPET_DIR" -type f | sed "s|$SNIPPET_DIR||" | fzf)
-cat "$HOME/.config/helix/snippets/$file"
--- .config/helix/snippets/base.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  printf("Print");
-  return 0;
-}
